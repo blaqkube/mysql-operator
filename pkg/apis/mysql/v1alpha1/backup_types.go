61c24215b58c6dca695486bad0770922e91d5b18
blaqkube <blaqkube@users.noreply.github.com>
2020-05-12 22:31:03 +0200

    Create a simple resource to manage backups

---
+++ pkg/apis/mysql/v1alpha1/backup_types.go
@@ -0,0 +1,50 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type BackupExecutor struct {
+	Type string `json:"type,omitempty"`
+}
+
+// BackupSpec defines the desired state of Backup
+type BackupSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Executor BackupExecutor `json:"executor,omitempty"`
+}
+
+// BackupStatus defines the observed state of Backup
+type BackupStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Backup is the Schema for the backups API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=backups,scope=Namespaced
+type Backup struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   BackupSpec   `json:"spec,omitempty"`
+	Status BackupStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// BackupList contains a list of Backup
+type BackupList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Backup `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Backup{}, &BackupList{})
+}
