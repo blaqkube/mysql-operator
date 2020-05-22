42d6433cc66f5fa5ff8547c4f183a125582d62a7
blaqkube <blaqkube@users.noreply.github.com>
2020-05-22 20:05:18 +0200

    Fix repository name

--- agent/go.mod
+++ agent/go.mod
@@ -1,4 +1,4 @@
-module github.com/GIT_USER_ID/GIT_REPO_ID
+module github.com/blaqkube/mysql-operator/agent
 
 go 1.13
 
--- agent/main.go
+++ agent/main.go
@@ -14,7 +14,7 @@ import (
 	"log"
 	"net/http"
 
-	openapi "github.com/GIT_USER_ID/GIT_REPO_ID/go"
+	openapi "github.com/blaqkube/mysql-operator/agent/go"
 )
 
 func main() {
