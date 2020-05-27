013dc0a3b0b0ff5458f839a17be5e130e33cbfea
blaqkube <64150653+blaqkube@users.noreply.github.com>
2020-05-27 15:56:48 +0200

    Move directories

---
+++ mysql-operator/pkg/apis/mysql/group.go
@@ -0,0 +1,6 @@
+// Package mysql contains mysql API versions.
+//
+// This file ensures Go source parsers acknowledge the mysql package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package mysql
