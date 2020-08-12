a2cc9ffed6f62900819e319779e2140b5cf428ff
blaqkube <64150653+blaqkube@users.noreply.github.com>
2020-08-12 22:43:21 +0200

    [operator] build and push controller 🔪 and operator ⛵

--- mysql-operator/.ci/build.sh
+++ mysql-operator/.ci/build.sh
@@ -4,6 +4,12 @@ set -e
 
 cd $GALLY_PROJECT_ROOT
 export AGENT_VERSION=$(gally list -p agent | grep GALLY_PROJECT_VERSION | cut -d'"' -f4)
-docker build --build-arg agent_version=$AGENT_VERSION -t $TAG:$GALLY_PROJECT_VERSION .
-docker push $TAG:$GALLY_PROJECT_VERSION
-cd $GALLY_ROOT
+export VERSION=$GALLY_PROJECT_VERSION
+export IMG=quay.io/blaqkube/mysql-controller:$GALLY_PROJECT_VERSION
+export BUNDLE_IMG=quay.io/blaqkube/mysql-operator:$GALLY_PROJECT_VERSION
+make docker-build
+make docker-push
+make bundle
+make bundle-build
+make bundle-push
+
--- mysql-operator/controllers/user_controller.go
+++ mysql-operator/controllers/user_controller.go
@@ -46,6 +46,98 @@ func (r *UserReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	return ctrl.Result{}, nil
 }
 
+/*
+func (r *ReconcileUser) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling User")
+
+	// Fetch the User instance
+	instance := &mysqlv1alpha1.User{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	// Check if this Pod already exists
+	pod := &corev1.Pod{}
+	err = r.client.Get(
+		context.TODO(),
+		types.NamespacedName{
+			Name:      instance.Spec.Instance + "-0",
+			Namespace: instance.Namespace,
+		},
+		pod,
+	)
+	if err != nil {
+		time := metav1.Now()
+		condition := mysqlv1alpha1.ConditionStatus{
+			LastProbeTime: &time,
+			Status:        "Failed",
+			Message:       fmt.Sprintf("Cannot find pod %s-0; error: %v", instance.Spec.Instance, err),
+		}
+		instance.Status.LastCondition = "Failed"
+		instance.Status.Conditions = []mysqlv1alpha1.ConditionStatus{condition}
+		err = r.client.Status().Update(context.TODO(), instance)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+		return reconcile.Result{}, nil
+	}
+	cfg := agent.NewConfiguration()
+	cfg.BasePath = "http://" + pod.Status.PodIP + ":8080"
+	// cfg.BasePath = "http://localhost:8080"
+	api := agent.NewAPIClient(cfg)
+	user := agent.User{
+		Username: instance.Spec.Username,
+		Password: instance.Spec.Password,
+		Grants:   []agent.Grant{},
+	}
+	for _, v := range instance.Spec.Grants {
+		user.Grants = append(user.Grants, agent.Grant{
+			Database:   v.Database,
+			AccessMode: v.AccessMode,
+		})
+	}
+	_, _, err = api.MysqlApi.CreateUser(context.TODO(), user, nil)
+	if err != nil {
+		time := metav1.Now()
+		condition := mysqlv1alpha1.ConditionStatus{
+			LastProbeTime: &time,
+			Status:        "Failed",
+			Message:       fmt.Sprintf("Error accessing api: %v", err),
+		}
+		instance.Status.LastCondition = "Failed"
+		instance.Status.Conditions = []mysqlv1alpha1.ConditionStatus{condition}
+		err = r.client.Status().Update(context.TODO(), instance)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+		return reconcile.Result{}, nil
+	}
+	t := metav1.Now()
+	condition := mysqlv1alpha1.ConditionStatus{
+		LastProbeTime: &t,
+		Status:        "Succeeded",
+		Message:       "User " + instance.Spec.Username + " created",
+	}
+	instance.Status.LastCondition = "Succeeded"
+	instance.Status.Conditions = []mysqlv1alpha1.ConditionStatus{condition}
+	err = r.client.Status().Update(context.TODO(), instance)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	return reconcile.Result{}, nil
+}
+*/
+
 func (r *UserReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&mysqlv1alpha1.User{}).
