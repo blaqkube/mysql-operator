7a227d939db870b380dba9285811de4eaf849a7c
blaqkube <64150653+blaqkube@users.noreply.github.com>
2020-08-13 03:36:22 +0200

    Bundle SEMVER version

--- mysql-operator/.ci/build.sh
+++ mysql-operator/.ci/build.sh
@@ -6,12 +6,10 @@ cd $GALLY_PROJECT_ROOT
 export AGENT_VERSION=$(gally list -p agent | grep GALLY_PROJECT_VERSION | cut -d'"' -f4)
 export VERSION=$GALLY_PROJECT_VERSION
 export IMG=quay.io/blaqkube/mysql-controller:$GALLY_PROJECT_VERSION
-export BUNDLE_IMG=quay.io/blaqkube/mysql-operator:$GALLY_PROJECT_VERSION
+export BUNDLE_IMG=quay.io/blaqkube/mysql-operator:$BUNDLE_VERSION
 make docker-build
 make docker-push
 make bundle
 git status -s
 git diff
 make bundle-build
-make bundle-push
-
