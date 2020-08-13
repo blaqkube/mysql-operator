b0c5dc189bc8e969fa9e99c60db98b99153e7b4d
blaqkube <64150653+blaqkube@users.noreply.github.com>
2020-08-13 03:42:30 +0200

    Add version to the controller too

--- mysql-operator/.ci/build.sh
+++ mysql-operator/.ci/build.sh
@@ -4,9 +4,8 @@ set -e
 
 cd $GALLY_PROJECT_ROOT
 export AGENT_VERSION=$(gally list -p agent | grep GALLY_PROJECT_VERSION | cut -d'"' -f4)
-export VERSION=$GALLY_PROJECT_VERSION
-export IMG=quay.io/blaqkube/mysql-controller:$GALLY_PROJECT_VERSION
-export BUNDLE_IMG=quay.io/blaqkube/mysql-operator:$BUNDLE_VERSION
+export IMG=quay.io/blaqkube/mysql-controller:$VERSION
+export BUNDLE_IMG=quay.io/blaqkube/mysql-operator:$VERSION
 make docker-build
 make docker-push
 make bundle
