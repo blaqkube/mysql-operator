e7b72049b71b4ea0bf12462c8e1f11e835dc3993
blaqkube <64150653+blaqkube@users.noreply.github.com>
2020-12-28 00:30:26 +0100

    [ops] prepare automatic 👾 bundle from tag

--- mysql-operator/.ci/bundle.sh
+++ mysql-operator/.ci/bundle.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+echo "no bundle for $GALLY_PROJECT_NAME"
+echo "  * CIRCLE_TAG: $CIRCLE_TAG"
+echo "  * CIRCLE_SHA1: $CIRCLE_SHA1"
+exit 0
+
 cd $GALLY_PROJECT_ROOT
 export AGENT_VERSION=$(gally list -p agent | grep GALLY_PROJECT_VERSION | cut -d'"' -f4)
 export IMG=quay.io/blaqkube/mysql-controller:$VERSION
