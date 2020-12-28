e04eff998f6cfb17653ab2962b442407f8ea06b7
blaqkube <64150653+blaqkube@users.noreply.github.com>
2020-12-29 00:29:40 +0100

    [ops] Add 🦏 index build to the tag flow (#119)

--- mysql-operator/.ci/bundle.sh
+++ mysql-operator/.ci/bundle.sh
@@ -6,6 +6,7 @@ echo "no bundle for $GALLY_PROJECT_NAME"
 echo "  * CIRCLE_TAG:  $CIRCLE_TAG"
 echo "  * CIRCLE_SHA1: $CIRCLE_SHA1"
 echo "  * VERSION:     $VERSION"
+echo "  * PREVIOUS:    $PREV_VERSION"
 
 if [[ "v${VERSION}" != "$CIRCLE_TAG" ]]; then
   echo "Error, only tag v${VERSION} is allowed"
@@ -21,6 +22,12 @@ if [[ "${AGENT_CODE}" != "${AGENT_VERSION}" ]]; then
   exit 1
 fi
 
+# export EXISTS=$(grep "  replaces: $PREV_VERSION" config/manifests/bases/mysql-operator.clusterserviceversion.yaml -c)
+# if [[ "${EXISTS}" != "1" ]]; then
+#   echo "Previous bundle version is not in CSV..."
+#   exit 1
+# fi
+
 export AGENT_IMG=quay.io/blaqkube/mysql-agent:${AGENT_CODE}
 docker pull $AGENT_IMG
 docker tag $AGENT_IMG quay.io/blaqkube/mysql-agent:$VERSION
@@ -32,4 +39,14 @@ make docker-push
 make bundle
 git status -s | cat
 make bundle-build
-docker push $BUNDLE_IMG 
+docker push $BUNDLE_IMG
+
+docker pull quay.io/blaqkube/mysql-operator:$VERSION
+# opm index add --container-tool docker \
+#   --bundles quay.io/blaqkube/mysql-operator:$VERSION \
+#   --from-index quay.io/blaqkube/operators-index:$PREV_VERSION
+#   --tag quay.io/blaqkube/operators-index:$VERSION
+opm index add --container-tool docker \
+  --bundles quay.io/blaqkube/mysql-operator:$VERSION \
+  --tag quay.io/blaqkube/operators-index:$VERSION
+docker push quay.io/blaqkube/operators-index:$VERSION
