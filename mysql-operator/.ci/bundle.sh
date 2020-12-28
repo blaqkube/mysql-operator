635a4fa4dc0b41c8bcec72daddf439611fb604ac
blaqkube <64150653+blaqkube@users.noreply.github.com>
2020-12-28 09:30:00 +0100

    [ops] fix issue with 💜 docker pull

--- mysql-operator/.ci/bundle.sh
+++ mysql-operator/.ci/bundle.sh
@@ -22,6 +22,7 @@ if [[ "${AGENT_CODE}" != "${AGENT_VERSION}" ]]; then
 fi
 
 export AGENT_IMG=quay.io/blaqkube/mysql-agent:${AGENT_CODE}
+docker pull $AGENT_IMG
 docker tag $AGENT_IMG quay.io/blaqkube/mysql-agent:$VERSION
 docker push quay.io/blaqkube/mysql-agent:$VERSION
 export IMG=quay.io/blaqkube/mysql-controller:$VERSION
