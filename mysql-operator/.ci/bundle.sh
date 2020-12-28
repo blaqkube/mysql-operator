dd23eb7e1731da0cb780a208ba9fd1436efcfba7
blaqkube <64150653+blaqkube@users.noreply.github.com>
2020-12-28 08:13:05 +0100

    [ops] automatic 👾 bundle from git tag (#114)
    This is a change to the CI pipeline. It now supports building the controller and operator on quay.io from a version tag

--- mysql-operator/.ci/bundle.sh
+++ mysql-operator/.ci/bundle.sh
@@ -1,9 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+echo "no bundle for $GALLY_PROJECT_NAME"
+echo "  * CIRCLE_TAG:  $CIRCLE_TAG"
+echo "  * CIRCLE_SHA1: $CIRCLE_SHA1"
+echo "  * VERSION:     $VERSION"
+
+if [[ "v${VERSION}" != "$CIRCLE_TAG" ]]; then
+  echo "Error, only tag v${VERSION} is allowed"
+  exit 1
+fi
+
+echo "Building version v${VERSION}..."
 cd $GALLY_PROJECT_ROOT
+export AGENT_CODE=$(grep "DefaultAgentVersion =" main.go | cut -d '"' -f2)
 export AGENT_VERSION=$(gally list -p agent | grep GALLY_PROJECT_VERSION | cut -d'"' -f4)
+if [[ "v${VERSION}" == "CIRCLE_TAG" ]]; then
 export IMG=quay.io/blaqkube/mysql-controller:$VERSION
 export BUNDLE_IMG=quay.io/blaqkube/mysql-operator:$VERSION
 make docker-build
@@ -11,4 +24,4 @@ make docker-push
 make bundle
 git status -s | cat
 make bundle-build
-docker push $BUNDLE_IMG
+docker push $BUNDLE_IMG 
